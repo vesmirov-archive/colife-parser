34e874a1ef18d85cba53b98c58742764cedb1d69
Evan Vilagov <evan.vilagov@gmail.com>
2021-10-18 01:02:44 +0300

    Created simple parser

--- colife.py
+++ colife.py
@@ -1,35 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
-
 from drivers import Firefox
+from parser.objects import get_colife_objects
 
 OBJECTS_READY_URL = 'https://colife.ru/catalog'
 OBJECTS_IN_RENOVATION_URL = 'https://colife.ru/remont'
 
-READY_HTML_PATH = {
-    'div': {'id': 'rec118977214', 'order': None},
-    'div': {'class': 't776', 'order': 0},
-    'div': {'class': 't-store js-store', 'order': 0},
-    'div': {'class': 'js-store-grid-cont t-store__grid-cont t-container t-store__grid-cont_mobile-grid t-store__valign-buttons', 'order': 2},
-}
-
-RENOVATION_HTML_PATH = {}
-
-
-def get_colife_objects(html, path, parser='lxml'):
-    warnings = []
-    soup = BeautifulSoup(html, parser)
-
-    # TODO add scrapping options    
+UNIQUE_HTML_ELEMENT = {'element': 'div', 'attribute': 'data-product-pack-label', 'value': 'lwh'}
 
 
 def main():
-    browser = Firefox()
-    
-    page_html = browser.get(OBJECTS_READY_URL)
-    objets = get_colife_objects(page_html, READY_HTML_PATH)
-
+    browser = Firefox()    
+    browser.get(OBJECTS_READY_URL)
+    page_html = browser.page
     browser.close()
+
+    objects = get_colife_objects(page_html, UNIQUE_HTML_ELEMENT)
+
+    for obj in objects:
+        print(obj)
 
 
 if __name__ == '__main__':
